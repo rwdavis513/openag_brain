72a784e0f0008438395de86b01d5d64c14e2cb72
Bob Davis <bob.davis@fenome.com>
2017-04-25 14:19:45 -0600

    Moved from RPi.GPIO to periphery GPIO library

--- nodes/signal_shutdown.py
+++ nodes/signal_shutdown.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # Simple script for shutting down the raspberry Pi at the press of a button.
 # by Inderpreet Singh
-# Adapted by Jake Rye and Bob Davis
+# Adapted by Jake Rye and Bob Davis in April, 2017
 
-import RPi.GPIO as GPIO
+from periphery import GPIO
 import time
 import os
 import logging
@@ -20,27 +20,26 @@ def setup_gpio_pins():
     # Use the Broadcom SOC Pin numbers
     # Setup the Pin with Internal pullups enabled and PIN in reading mode.
     rospy.logdebug('Setting GPIO27 / PIN13 to INPUT PULLUP')
-    GPIO.setmode(GPIO.BOARD)
     # PIN13 / BCM 27 YELLOW / SW-COM - Signals Shutdown
-    GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+    gpio_13 = GPIO(13, "in")  # Might need to pull the pin up?
 
     # Turn delay relay on
     # Sends signal to the relay to stay on until this drops low.
     rospy.logdebug('Setting GPIO22 / PIN15 to OUTPUT HIGH')
-    GPIO.setup(15, GPIO.OUT) # PIN15 / BCM 22 / WHITE / CH1
-    GPIO.output(15, GPIO.HIGH)
+    gpio_15 = GPIO(15, "out") # PIN15 / BCM 22 / WHITE / CH1
 
 
-def check_for_shutdown(pub13):
+def check_for_shutdown():
     # Monitor pin for stable signal to safely shutdown
     while not rospy.is_shutdown():
-        pub13.publish(GPIO.input(13))
-        if not GPIO.input(13):
+        shutdown_signal_pin = gpio_13.read()
+        rospy.logdebug(shutdown_signal_pin)
+        if not shutdown_signal_pin:
             rospy.logdebug('Initiating safe shutdown sequence')
             successful_debounce = True
             input_sum = 0
             for i in range(VERIFICATION_TIME_SEC * 1000):
-                input_sum += GPIO.input(13)
+                input_sum += gpio_13.read()
                 time.sleep(0.001)
             avg_signal = input_sum / ( VERIFICATION_TIME_SEC * 1000)
             rospy.logdebug('The average signal after {} seconds was {}'.format(
@@ -54,7 +53,7 @@ def check_for_shutdown(pub13):
                 rospy.logdebug('Safely shutting down')
                 rospy.signal_shutdown('Safely shutting down due to Power Off Button')
                 time.sleep(5)
-                GPIO.output(15, GPIO.LOW)
+                gpio_15.write(0)
                 os.system("sudo shutdown -h now")
                 break
         rospy.sleep(1.)
@@ -62,9 +61,8 @@ def check_for_shutdown(pub13):
 
 if __name__ == '__main__':
     rospy.init_node('signal_shutdown')
-    pub13 = rospy.Publisher("/GPIO/13", Float64, queue_size=10)
     setup_gpio_pins()
     try:
-        check_for_shutdown(pub13)
+        check_for_shutdown()
     except rospy.ROSInterruptException:
         pass
