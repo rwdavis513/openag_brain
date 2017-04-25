7b5d4100f0c6c802767d33c47d87891ca634c3a1
Bob Davis <bob.davis@fenome.com>
2017-04-25 16:07:08 -0600

    Added test case for signal shutdown

---
+++ test/test_nodes/test_signal_shutdown.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+PKG = 'openag_brain'
+NAME = 'test_signal_shutdown'
+
+import sys
+import unittest
+import rospy
+from std_msgs.msg import Float64
+
+from nodes.signal_shutdown import setup_gpio_pins, check_for_shutdown
+
+class TestSignalShutdown(unittest.TestCase):
+    """
+    Test the signal shutdown module to see if it starts up without errors
+    """
+
+    def test_setup(self):
+        setup_gpio_pins()
+
+    def test_signal_shutdown(self):
+        check_for_shutdown()
+
+if __name__ == '__main__':
+    unittest.main()
