946fbddca5898de55b00a21251272e72900b9794
Gordon <gordonbrander@users.noreply.github.com>
2017-05-05 17:07:42 -0600

    Updated to address issues with python-periphery package not setting up pins correctly. It isn't functional because initiating a pin as low

--- nodes/signal_shutdown.py
+++ nodes/signal_shutdown.py
@@ -4,6 +4,7 @@
 # Adapted by Jake Rye and Bob Davis in April, 2017
 
 from periphery import GPIO
+from periphery.gpio import GPIOError
 import time
 import os
 import logging
@@ -15,18 +16,41 @@ VERIFICATION_TIME_SEC = 3          # Time to wait to verify shutdown signal
 SHUTDOWN_SIGNAL_THRESHOLD = 0.98   # Percent of shutdown signals needed to
                                    # signal a shutdown (ranges from 0 to 1)
 
+def setup_pin(pin, direction, number_of_attempts=5):
+    """
+    The first attempt to connect to the GPIO pins fails, but the second try succeeds. It appears to be a timing issue in the periphery library.
+       It first exports to the GPIO pin to connect it and then writes the direction. Writing the direction fails, 
+       because of a permission error.
+    This function is a work around to try several times to allow the export function to finish creating the class in
+       /sys/class/gpio/gpio{pin_number}
+       http://python-periphery.readthedocs.io/en/latest/_modules/periphery/gpio.html#GPIO
+    returns gpio object for writing or reading.
+    """
+    for _ in range(number_of_attempts):
+       try:
+           gpio_pin = GPIO(pin, direction)
+           break
+       except GPIOError as e:
+           rospy.logdebug("Failed to connect to GPIO pin {}. {} attempt.".format(pin, _))
+           if _+1 == number_of_attempts:
+               raise e
+           time.sleep(0.05)
+       rospy.loginfo("GPIO Pin {} took {} tries to connect.".format(pin, _ + 1))
+    return gpio_pin
+
 
 def setup_gpio_pins():
     # Use the Broadcom SOC Pin numbers
     # Setup the Pin with Internal pullups enabled and PIN in reading mode.
     rospy.logdebug('Setting GPIO27 / PIN13 to INPUT PULLUP')
     # PIN13 / BCM 27 YELLOW / SW-COM - Signals Shutdown
-    gpio_13 = GPIO(13, "in")  # Might need to pull the pin up?
+    gpio_13 = setup_pin(13, "high")  # Pull pin high before watching if it drops low to signal shutdown
+    gpio_13 = setup_pin(13, "in")  # Initializes pin to low, not sure how to set to high and watch to drop low.
 
     # Turn delay relay on
     # Sends signal to the relay to stay on until this drops low.
     rospy.logdebug('Setting GPIO22 / PIN15 to OUTPUT HIGH')
-    gpio_15 = GPIO(15, "out") # PIN15 / BCM 22 / WHITE / CH1
+    gpio_15 = setup_pin(15, "out") # PIN15 / BCM 22 / WHITE / CH1
     return gpio_13, gpio_15
 
 
@@ -34,6 +58,7 @@ def check_for_shutdown(gpio_13, gpio_15):
     # Monitor pin for stable signal to safely shutdown
     while not rospy.is_shutdown():
         shutdown_signal_pin = gpio_13.read()
+        rospy.loginfo(shutdown_signal_pin)
         rospy.logdebug(shutdown_signal_pin)
         if not shutdown_signal_pin:
             rospy.logdebug('Initiating safe shutdown sequence')
@@ -54,7 +79,7 @@ def check_for_shutdown(gpio_13, gpio_15):
                 rospy.logdebug('Safely shutting down')
                 rospy.signal_shutdown('Safely shutting down due to Power Off Button')
                 time.sleep(5)
-                gpio_15.write(0)
+                gpio_15.write(False)
                 os.system("sudo shutdown -h now")
                 break
         rospy.sleep(1.)
