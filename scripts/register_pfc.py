70412b4d06722eb773d08f1acce6ffcb871527de
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-06-06 19:42:36 +0000

    Added function to register pfc and fixed a few bugs so it works now

--- scripts/register_pfc.py
+++ scripts/register_pfc.py
@@ -1,15 +1,22 @@
-import os
+import os, time
 import requests
 import subprocess
 import json
 import pdb
 import pexpect
+import random
+import uuid
 from settings import SERVER_URL, SSH_FOLDER, user_account
 
 login_info = {
         "password": user_account['password'],
         "email": user_account['email']
         }
+
+# TODO: Add Tests for each function
+# TODO: Add check to account for RSA Fingerprint text
+# TODO: Modify UUID to a hash of the mac address or something unique to the pfc
+# TOOD: Modify the functions to be pure functions so they don't modify the session variable
 
 
 def get_tunnel_info():
@@ -26,15 +33,29 @@ def get_tunnel_info():
         return new_tunnel_info
 
 
-def register_login(session):
+def register_login(session, register_user=False):
+    """
+    Logs the User into the cloud software
+    """
     REGISTER_URL = "/auth"
-    response = session.post(SERVER_URL + REGISTER_URL, json=login_info)
-    if 200 <= response.status_code <= 210:
-        return True
+    if register_user:
+        # PUT Registers a NEW user
+        response = session.put(SERVER_URL + REGISTER_URL, json=login_info)
+        if 200 <= response.status_code <= 210:
+            return True
+        else:
+            print(response.text)
+            pdb.set_trace()
+            raise Exception("Failed to Register")
     else:
-        pdb.set_trace()
-        print(response.text)
-        raise Exception("Failed to Register")
+        # POST Logs the User in
+        response = session.post(SERVER_URL + REGISTER_URL, json=login_info)
+        if 200 <= response.status_code <= 210:
+            return True
+        else:
+            print(response.text)
+            pdb.set_trace()
+            raise Exception("Failed to Login")
 
 
 def get_new_tunnel_info(session):
@@ -43,8 +64,8 @@ def get_new_tunnel_info(session):
     if response.status_code == 201:
         return response.json()
     else:
-        #pdb.set_trace()
         print(response.text)
+        pdb.set_trace()
         raise Exception("Error: Resceived error code {}".format(response.status_code))
 
 
@@ -65,7 +86,12 @@ def check_for_tunnel_info(file_name='tunnel_info.json'):
 
 
 def create_ssh_tunnel(tunnel_command, password):
-
+    """ Creates a Reverse SSH Tunnel to allow the Cloud to talk to the PFC
+    Need to account for the RSA Fingerprint prompt: 
+    The authenticity of host '[ec2-34-209-193-14.us-west-2.compute.amazonaws.com]:3004 ([34.209.193.14]:3004)' can't be established.
+    RSA key fingerprint is 67:54:e8:e4:d8:ae:e0:61:11:1e:f5:5c:77:d6:83:de.
+    Are you sure you want to continue connecting (yes/no)? yes
+    """
     try:
         ssh_tunnel = pexpect.spawn(tunnel_command)
         ssh_tunnel.expect('password:')
@@ -78,11 +104,67 @@ def create_ssh_tunnel(tunnel_command, password):
         print(str(e))
 
 
+def get_pfc_uuid():
+    return str(uuid.uuid1())
+
+
+def get_pfc_name():
+    pfc_name = os.uname()[1]
+    if not pfc_name:
+        pfc_name = "pfc" + str(random.randint(0, 1000))
+    return pfc_name
+
+
+def add_pfc_to_cloud(session, pfc_url):
+    """
+    Registers the PFC by submitting PFC information to cloud backend.
+
+    Data Required to register pfc.
+    {
+      "url": "string",
+      "name": "string",
+      "uuid": "string"
+    }
+    """
+    pfc_data = {}
+    pfc_data['url'] = pfc_url   # Provided by cloud backend on ssh tunnel call
+    pfc_data['name'] = get_pfc_name()   # Return the PFCs host name
+    pfc_data['uuid'] = get_pfc_uuid()   # Return the PFCs UUID based on the Mac Address and Serial Number (hostname)
+    API_REGISTRATION_END_POINT = "/pfc"
+    session.put(SERVER_URL + API_REGISTRATION_END_POINT, json=pfc_data)
+    # Add Error checking
+
+def add_pfc(tunnel_info, session=None):
+    """
+    Adds a PFC to the Users Account
+    """
+    if not session:
+        raise Exception("Requires requests session object to be passed in")
+    register_login(session, register_user=False)  # Check if already logged in
+    add_pfc_to_cloud(session, tunnel_info['url'])
+
+
 def main():
     tunnel_info = get_tunnel_info()
     print(tunnel_info)
     save_tunnel_info(tunnel_info)
+    # Check for ssh tunnel
+    # Create Tunnel if it doesn't exist
     create_ssh_tunnel(tunnel_info['ssh_command'], tunnel_info['password'])
+    # Register PFC
+
+
+def test_add_pfc():
+    session = requests.Session()
+    tunnel_info = get_tunnel_info()
+    add_pfc(tunnel_info, session)
+
+
+def test_get_pfc_name():
+    pfc_name = get_pfc_name()
+    assert type(pfc_name) == str
+    assert len(pfc_name) < 20
+
 
 if __name__ == "__main__":
     main()
